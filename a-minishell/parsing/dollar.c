68f78abfa75cd6f48477ee1c33d3c129b1824d99
Ahmed Radwan <aradwan@lab1r6s10.42abudhabi.ae>
2025-09-08 19:16:22 +0400

    fa11333444

--- a-minishell/parsing/dollar.c
+++ a-minishell/parsing/dollar.c
@@ -6,7 +6,7 @@
 /*   By: aradwan <aradwan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 17:23:19 by aradwan           #+#    #+#             */
-/*   Updated: 2025/09/08 19:14:16 by aradwan          ###   ########.fr       */
+/*   Updated: 2025/09/08 19:16:05 by aradwan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,28 +82,28 @@ int	expand(char **str, char *expanded, t_variables *v)
 	return (0);
 }
 
-void	generate_strings_helper(char **str, char *expanded, t_variables *var)
+void	generate_strings_helper(char **str, char *expanded, t_variables *v)
 {
 	char	*joined;
 	char	*joined2;
 
-	if (!var->in_d_quotes)
+	if (!v->in_d_quotes)
 	{
 		joined = ft_strjoin(expanded, "\"");
 		joined2 = ft_strjoin("\"", joined);
-		*str = storing(*str, var->i - var->len - 1, var->len + 1, joined2);
-		var->i += ft_strlen(expanded) - var->len - 1;
+		*str = storing(*str, v->i - v->len - 1, v->len + 1, joined2);
+		v->i += ft_strlen(expanded) - v->len - 1;
 		free(joined2);
 		free(joined);
 	}
 	else
 	{
-		*str = storing(*str, var->i - var->len - 1, var->len + 1, expanded);
-		var->i += ft_strlen(expanded) - var->len - 1;
+		*str = storing(*str, v->i - v->len - 1, v->len + 1, expanded);
+		v->i += ft_strlen(expanded) - v->len - 1;
 	}
 }
 
-int	generate_string(char **str, char **tmp, t_variables *var, t_shell *pipe)
+int	generate_string(char **str, char **tmp, t_variables *v, t_shell *pipe)
 {
 	char	*expanded;
 
@@ -113,12 +113,12 @@ int	generate_string(char **str, char **tmp, t_variables *var, t_shell *pipe)
 		expanded = my_getenv(*tmp, pipe);
 		if (expanded)
 		{
-			generate_strings_helper(str, expanded, var);
+			generate_strings_helper(str, expanded, v);
 		}
 		else if (!expanded)
 		{
-			*str = storing(*str, var->i - var->len - 1, var->len + 1, "");
-			var->i -= var->len + 1;
+			*str = storing(*str, v->i - v->len - 1, v->len + 1, "");
+			v->i -= v->len + 1;
 		}
 		free(*tmp);
 		return (1);
@@ -128,26 +128,26 @@ int	generate_string(char **str, char **tmp, t_variables *var, t_shell *pipe)
 
 void	dollar_expansion(char **str, t_shell *pipe)
 {
-	t_variables	var;
+	t_variables	v;
 	char	*tmp;
 	char	*expanded;
 
-	var.len = 0;
+	v.len = 0;
 	expanded = NULL;
-	var.i = 0;
-	var.in_d_quotes = 0;
-	var.in_quotes = 0;
-	while ((*str)[var.i])
+	v.i = 0;
+	v.in_d_quotes = 0;
+	v.in_quotes = 0;
+	while ((*str)[v.i])
 	{
-		quotes_check(str, &var);
-		if ((*str)[var.i] == '$' && !var.in_quotes && (*str)[var.i + 1])
+		quotes_check(str, &v);
+		if ((*str)[v.i] == '$' && !v.in_quotes && (*str)[v.i + 1])
 		{
-			if (expand(str, expanded, &var))
+			if (expand(str, expanded, &v))
 				continue ;
-			tmp = ft_substr(*str, var.i - var.len, var.len);
-			if (generate_string(str, &tmp, &var, pipe))
+			tmp = ft_substr(*str, v.i - v.len, v.len);
+			if (generate_string(str, &tmp, &v, pipe))
 				continue ;
 		}
-		var.i++;
+		v.i++;
 	}
 }

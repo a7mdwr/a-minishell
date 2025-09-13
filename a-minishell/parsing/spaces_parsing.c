16d1b1d492bb68a96a41132a4f81879964de4647
Ahmed Radwan <aradwan@lab1r3s2.42abudhabi.ae>
2025-09-13 18:02:25 +0400

    remove the double pointer from spaces parsing file becuse its unused

--- a-minishell/parsing/spaces_parsing.c
+++ a-minishell/parsing/spaces_parsing.c
@@ -6,41 +6,41 @@
 /*   By: aradwan <aradwan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 14:31:19 by aradwan           #+#    #+#             */
-/*   Updated: 2025/09/13 17:13:46 by aradwan          ###   ########.fr       */
+/*   Updated: 2025/09/13 17:58:53 by aradwan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static void	add_space(int *j, char **new_str, int type)
+static void	add_space(int *j, char *new_str, int type)
 {
-	(*new_str)[(*j)++] = ' ';
+	new_str[(*j)++] = ' ';
 	if (type == 0)
-		(*new_str)[(*j)++] = '<';
+		new_str[(*j)++] = '<';
 	else if (type == 1)
 	{
-		(*new_str)[(*j)++] = '<';
-		(*new_str)[(*j)++] = '<';
+		new_str[(*j)++] = '<';
+		new_str[(*j)++] = '<';
 	}
 	else if (type == 2)
-		(*new_str)[(*j)++] = '>';
+		new_str[(*j)++] = '>';
 	else if (type == 3)
 	{
-		(*new_str)[(*j)++] = '>';
-		(*new_str)[(*j)++] = '>';
+		new_str[(*j)++] = '>';
+		new_str[(*j)++] = '>';
 	}
 	else if (type == 4)
-		(*new_str)[(*j)++] = ' ';
-	(*new_str)[(*j)++] = ' ';
+		new_str[(*j)++] = ' ';
+	new_str[(*j)++] = ' ';
 }
 
-static void	add_spaces_helper(char **input, int *i, int *j, char **new_input)
+static void	add_spaces_helper(char *input, int *i, int *j, char *new_input)
 {
 	char c;
 	char next;
 
-	c = (*input)[*i];
-	next = (*input)[*i + 1];
+	c = input[*i];
+	next = input[*i + 1];
 	if (c == '<' && next != '<')
 		add_space(j, new_input, 0);
 	else if (c == '<' && next == '<')
@@ -58,7 +58,7 @@ static void	add_spaces_helper(char **input, int *i, int *j, char **new_input)
 	else if (c == '\t')
 		add_space(j, new_input, 4);
 	else
-		(*new_input)[(*j)++] = c;
+		new_input[(*j)++] = c;
 }
 
 char	*ft_add_spaces(char *input)
@@ -79,7 +79,7 @@ char	*ft_add_spaces(char *input)
 		v.i = i;
 		quotes_check(&input, &v);
 		if (!v.in_quotes && !v.in_d_quotes)
-			add_spaces_helper(&input, &i, &j, &new_input);
+			add_spaces_helper(input, &i, &j, new_input);
 		else
 			new_input[j++] = input[i];
 	}
